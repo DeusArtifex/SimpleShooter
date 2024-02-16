4131da988ba7e0a04c74d6b739d793b72a819b90
DeusArtifex <deus3artifex@gmail.com>
2024-02-16 15:08:45 +0100

    Add gun actor and handle its spawning

--- Source/SimpleShooter/ShooterCharacter.h
+++ Source/SimpleShooter/ShooterCharacter.h
@@ -6,6 +6,8 @@
 #include "GameFramework/Character.h"
 #include "ShooterCharacter.generated.h"
 
+class AGun;
+
 UCLASS()
 class SIMPLESHOOTER_API AShooterCharacter : public ACharacter
 {
@@ -35,5 +37,10 @@ private:
 
 	UPROPERTY(EditAnywhere)
 	float RotationRate;
+
+	UPROPERTY(EditDefaultsOnly)
+	TSubclassOf<AGun> GunClass;
+	UPROPERTY()
+	AGun* Gun;
 
 };
